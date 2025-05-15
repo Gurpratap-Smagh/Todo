4a31b9d9afbf5ac83e19e0451f208adf39e23852
Gurpratap-Smagh <gurpratapkarnsmagh@gmail.com>
2025-05-15 05:10:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');  // Import CORS package
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all requests
 app.use(cors());
